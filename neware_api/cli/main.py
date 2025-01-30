fa7816a7f6e5253cd613c4763b789a6ff0f26a14
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-01-30 09:56:48 +0100

    Add get_status function to CLI

--- neware_api/cli/main.py
+++ neware_api/cli/main.py
@@ -1,6 +1,11 @@
 """CLI for the Neware battery cycling API."""
 
+import json
+from typing import Annotated, Optional
+
 import typer
+
+from neware_api import NewareAPI
 
 app = typer.Typer()
 
@@ -12,9 +17,26 @@ def start() -> None:
 
 
 @app.command()
-def status() -> None:
-    """Get the status of the cycling process."""
-    typer.echo("Getting the status of the cycling process is NOT IMPLEMENTED YET.")
+def status(pipeline_ids: Annotated[Optional[list[str]], typer.Argument()] = None) -> None:  # noqa: UP007
+    """Get the status of the cycling process for all or selected pipelines.
+
+    Example usage:
+    >>> neware status
+    {"13-1-1": {"ip":127.0.0.1, "devtype": 27 ... }, "13-1-2": { ... }, ...
+    >>> neware status 13-1-5
+    {"13-1-5":{...}}
+    >>> neware status 13-1-5 14-3-5
+    {"13-1-5":{...}, "14-3-5":{...}}
+
+    Args:
+        pipeline_ids (optional, list[str]): list of pipeline IDs to get status from
+
+    Raises:
+        KeyError if pipeline ID not in channel map
+
+    """
+    with NewareAPI() as nw:
+        typer.echo(json.dumps(nw.get_status(pipeline_ids)))
 
 
 if __name__ == "__main__":
